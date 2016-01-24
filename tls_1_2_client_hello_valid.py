89dd6c7eefb99f91a2038a6da396aab39e682717
silverfoxy <babak.silverfox@gmail.com>
2016-01-24 11:36:03 +0330

    first commit

---
+++ tls_1_2_client_hello_valid.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Author : tintinweb@oststrom.com <github.com/tintinweb>
+
+import sys, os
+try:
+    import scapy.all as scapy
+except ImportError:
+    import scapy
+
+try:
+    # This import works from the project directory
+    basedir = os.path.abspath(os.path.join(os.path.dirname(__file__),"../"))
+    sys.path.append(basedir)
+    from scapy_ssl_tls.ssl_tls import *
+except ImportError:
+    # If you installed this package via pip, you just need to execute this
+    from scapy.layers.ssl_tls import *
+    
+import socket
+
+if __name__=="__main__":
+    if len(sys.argv)<=2:
+        print "USAGE: <host> <port>"
+        exit(1)
+        
+    target = (sys.argv[1],int(sys.argv[2]))
+    
+    # create tcp socket
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.connect(target)
+
+    # create TLS1.2 Handhsake / Client Hello packet
+    p = TLSRecord(version='TLS_1_2')/TLSHandshake()/TLSClientHello(version='TLS_1_2', cipher_suites=TLS_CIPHER_SUITES.keys())#, extensions=TLSExtension(type='signature_algorithms'))           
+    #p.show()
+
+    SSL(str(p)).show()
+    
+    print "sending TLS payload"
+    s.sendall(str(p))
+    resp = s.recv(8*1024)
+    print "received, %s"%repr(resp)
+    SSL(resp).show()
+    
+    s.close()
